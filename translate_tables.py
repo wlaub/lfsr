ea7f009b75b2adfc5b4740bbb2aa77cfaa2302de
wlaub <goldcray@gmail.com>
2019-08-31 17:33:25 -0700

    Generating lookup data as lists

--- translate_tables.py
+++ translate_tables.py
@@ -56,13 +56,185 @@ The first lookup table adds 16KB of mostly redundant data, but should make for f
 """
 import struct
 import sys
+import pprint
+from collections import OrderedDict
+
+def spread_equals(count, fill_range):
+    """
+    Fill the specified range with N spaces to return a list of the form
+    [fill_range[0], fill_range[0]+width/count]
+    Returns the right edge of the interval
+    """
+    width = fill_range[1]-fill_range[0]
+    a = fill_range[0]
+    result = [a+(i+1)*width/count for i in range(count)]
+    return result
+
+def spread_values(data : OrderedDict)->OrderedDict:
+    """
+    Data is of the form {value:count}
+    result is of the form {old_value:[new_value,...]}
+    Such that new_value is what the knob must be greater than to select the
+    corresponding option from old_value
+    for example:
+    The input dictionary
+    {
+    0  :1,
+    .5 :3,
+    1  :1,
+    }
+    indicates that there are values at knob positions 0, .5, and 1
+    (left, center, right), and that there is 1 value at left and right
+    and 3 values at center.
+    From 0 to 0.25, the knob should select the value for left.
+    From .25 to .75, the knob should select the value for center.
+        Since there are 3 values, they should be spread equally within the
+        interval: .25 -> .42 -> .58 -> .75
+    From .75 to 1, the knob should select the value for right.
+    The result dictionary is then
+    {
+    0 : [0],
+    .5: [.25,.42,.58],
+    1 : [.75],
+    }
+
+   
+    """
+    old_values = list(data.keys())
+
+    #new values maps old_values to a range [left, right]
+    new_values = [(old_values[0],[0])]
+    for prev_val, next_val in zip(old_values[:-1], old_values[1:]):
+        new_edge = (prev_val+next_val)/2
+        new_values[-1][1].append(new_edge)
+        new_values.append((next_val, [new_edge]))
+
+    new_values[-1][1].append(1)
+
+    result = OrderedDict(new_values)
+
+    for old_val, count in data.items():
+        new_val = result[old_val]
+        fill_range = list(new_val)
+        new_val.clear()
+        new_val.extend(spread_equals(count, fill_range))
+        pass
+
+    return result
+
+results = spread_values(OrderedDict([(0,1),(.5,3),(1,1)]))
+pprint.pprint(results)
+
+
+def generate_index(sequences, depth_index, max_depth):
+    """
+    Recursively generate a searchable index of data
+    """
+    param_values = []
+    param_counts = []
+    for seq in sequences:
+        taps = seq.taps
+        params = seq.params
+        if not params[depth_index] in param_values:
+            param_values.append(params[depth_index])
+            param_counts.append(1)
+        else:
+            param_counts[-1]+=1
+
+    param_values = list(zip(param_values, param_counts))
+
+    number = len(param_values)
+    terminal = depth_index >= max_depth-1
+
+    if not terminal:
+        spread_map = spread_values(OrderedDict([(x[0],1) for x in param_values]))
+    else:
+        spread_map = spread_values(OrderedDict(param_values))
+
+    result = []
+    if terminal:
+        access_value = -1
+        result.append(0)
+        result.append(len(sequences))
+        #0 | short
+        #len(sequences) | short
+        for seq in sequences:
+            taps = seq.taps
+            params = seq.params
+            #Generate index on the segment of sequences:
+            #mapping spread_map index to taps value
+            #compute length of resulting chunk
+            #return the resulting chunk
+            #at this point, the params[-1] will be a key mapping
+            #to the spread_values lists, so
+            #keep track of index and prev_value
+            #
+            #
+            if access_value != params[-1]:
+                index = 0
+                access_value = params[-1]
+
+            #data = list of 
+            #taps | short
+            #key | short
+            key = int(spread_map[access_value][index]*(2**16-1))
+            result.append(key)
+            result.append(taps)
+
+            index +=1
+
+        return result
+    else:
+        result.append(0)
+        result.append(len(param_values))
+        data = []
+        # 0 | short
+        # len(param_values) | short
+        index = 0
+        offset = len(param_values)*4+4 #TODO: Verify this
+        for value, count in param_values:
+            data_chunk = generate_index(sequences[index:index+count], depth_index+1, max_depth)
+            #header append:
+            #spread_map[value] | short
+            #offset | short
+            #
+            result.append(int(spread_map[value][0]*(2**16-1)))
+            result.append(offset)
+            data.extend(data_chunk)
+#            print(f'data: {data_chunk}')
+            #datamap append: data
+            index += count
+            offset+=len(data_chunk)*2
+ 
+
+            #Generate index over param_values pointing
+            #to indexes on sequences[idx:idx+count] for depth_index+1
+            #append subindex chunks into a list, then append that list
+            #to the the master index of this
+        #append index and data, return
+#        print(f'result: {result}')
+        result.extend(data)
+        return result
+
+
 
 class Sequence():
 
     def __init__(self, taps, params, norms):
         self.taps = taps
+        self.params = params
         self.params = [x/y for x,y in zip(params,norms)]
 
+
+    def _sort_key(self, reverse=False):
+        """
+        Return sorting tuple for this sequence. Reverse controls the
+        sorting order of the params
+        """
+        if not reverse:
+            return (*self.params, self.taps)
+        else:
+            return (*self.params[::-1], self.taps)
 
     def __repr__(self):
         return f'{self.taps}, {self.params}'
@@ -95,18 +267,96 @@ class SeqTable():
                 tdata.append(tseq)
 
 
+    def get_expanded_length_lookup(self):
+        """
+        Return an array mapping every possible sequence length to the best-fit
+        available sequence length using spread_values.
+
+        expanded_table is a list with at 4096 indices giving the best available
+        sequence length for the desired value. It will be used to populate the
+        sequence vector table
+        """
+        lengths = sorted(list(self.data.keys()))
+
+        length_map = spread_values(OrderedDict([(x,1) for x in lengths]))
+
+        length_map_items = list(length_map.items())
+        expanded_table = []
+        length_index = 0
+        for index in range(4096):
+            if index >= length_map_items[length_index+1][1][0]:
+                length_index += 1
+            expanded_table.append(length_map_items[length_index][0])
+
+        return expanded_table
+
+    def sort(self, reverse=False):
+        """
+        Go through and sort all the data
+        """
+        pass
+
+    def spread(self, reverse=False):
+        """
+        Go through and spread all the parameters for each sequence to space
+        them about their knobs on the range of 0 to 1. This will probably be
+        scaled to 4096 to export, but that's a separate step.
+
+
+        And go like param1 -> spread by counts of 1
+        for each param 1:
+                 param2 within param 1 -> spread by counts of taps
+
+        """
+        
+        #this appears to be incorrect
+        expanded_lookup = self.get_expanded_length_lookup()
+        pprint.pprint(expanded_lookup[:10])
+
+        lengths = sorted(list(self.data.keys()))
+        result = []
+        master_index = {}
+        offset = 0
+        for length in lengths:
+            sequences = sorted(self.data[length], key=lambda x: x._sort_key(reverse))
+
+            data_chunk = generate_index(sequences, 0, 2)
+            result.extend(data_chunk)
+            for desired, actual in enumerate(expanded_lookup):
+                if actual == length:
+                    master_index[desired] = offset
+            offset+=len(data_chunk)*2
+
+
+
+        return master_index, result
+
+
 if __name__ == '__main__':
     table = SeqTable("length_lookup_inv")
 
     print(f'{len(table.data.keys())}')
-    print(table.data[11])
+    pprint.pprint(
+            sorted(table.data[10], key=lambda x:x._sort_key())
+            )
+
+    master_idx, data = table.spread(reverse=True)
+    print(data[:100])
+    print(len(data))
+    print(len(master_idx))
 
 
-
-
-
-
-
+    address = int(master_idx[17]/2)
+    print(data[address:address+2])
+    for i in range(data[address+1]):
+        subaddress = int(data[address+3+i*2]/2)+address
+        data_range = data[address+i*2], data[address+2+i*2]
+        print(f'{i}: {data_range[0]} to {data_range[1]}: {data[subaddress:subaddress+2]}')
+        for j in range(data[subaddress+1]):
+            data_range = data[subaddress+j*2], data[subaddress+2+j*2]
+            taps = int(data[subaddress+3+j*2])
+            print(f'  {j}: {data_range[0]} to {data_range[1]} : {taps}')
+     
 
 
 
